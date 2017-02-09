e46c6fe616a0f291ee2222fa23e4c881e7006ea9
Mike Wilson <mwilson@brainfluid.ca>
2017-02-09 12:35:43 -0800

    Fix project structure

--- build.bat
+++ build.bat
@@ -21,17 +21,17 @@ REM - Option 1: Run dotnet build for every source folder in the project
 REM   e.g. call dotnet build <path> --configuration %config%
 REM - Option 2: Let msbuild handle things and build the solution
 REM "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MyLibrary.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call dotnet build src --configuration %config%
+call dotnet build src\DispatchRider --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 REM TODO: Bring these back
-REM call dotnet test test --configuration %config%
+REM call dotnet test test\DispatchRider --configuration %config%
 REM if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir %cd%\Artifacts
-call dotnet pack src --configuration %config% %version% --output Artifacts
+call dotnet pack src\DispatchRider --configuration %config% %version% --output Artifacts
 if not "%errorlevel%"=="0" goto failure
 
 :success
