a9e5a852b4e117e2f6f8f7622e44abfd3c1f13a5
Mike Wilson <mwilson@brainfluid.ca>
2017-03-17 09:45:10 -0700

    Switch to csproj format

--- build.bat
+++ build.bat
@@ -5,9 +5,9 @@ if "%config%" == "" (
 )
 
 set version=
-if not "%BuildCounter%" == "" (
-   set version=--version-suffix ci-%BuildCounter%
-)
+REM if not "%BuildCounter%" == "" (
+REM    set version=--version-suffix ci-%BuildCounter%
+REM )
 
 REM (optional) build.bat is in the root of our repo, cd to the correct folder where sources/projects are
 REM cd MyLibrary
@@ -21,8 +21,7 @@ REM - Option 1: Run dotnet build for every source folder in the project
 REM   e.g. call dotnet build <path> --configuration %config%
 REM - Option 2: Let msbuild handle things and build the solution
 REM "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MyLibrary.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call dotnet build src\DispatchRider --configuration %config%
-call dotnet build src\DispatchRider.AspNetCore --configuration %config%
+call dotnet build dispatchrider-dotnet.sln --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
@@ -33,8 +32,8 @@ REM if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir %cd%\Artifacts
-call dotnet pack src\DispatchRider --configuration %config% %version% --output Artifacts
-call dotnet pack src\DispatchRider.AspNetCore --configuration %config% %version% --output Artifacts
+call dotnet pack src\DispatchRider\DispatchRider.csproj --configuration %config% %version% --output Artifacts
+call dotnet pack src\DispatchRider.AspNetCore\DispatchRider.AspNetCore.csproj --configuration %config% %version% --output Artifacts
 if not "%errorlevel%"=="0" goto failure
 
 :success
